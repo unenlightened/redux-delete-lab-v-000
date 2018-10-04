042d32fa3c4888c4bd5d53d63a2cea51cda5de03
Chely Ho <adept_inept@yahoo.com>
2018-10-04 20:36:45 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -10,7 +10,7 @@ export default function manageBand(state = {
       return { ...state, bands: [...state.bands, band] }
 
     case 'DELETE_BAND':
-      return { bands: state.bands.filter(band => band.id !== action.id) }
+      return { ...state, bands: state.bands.filter(band => band.id !== action.id) }
     default:
       return state;
   }
