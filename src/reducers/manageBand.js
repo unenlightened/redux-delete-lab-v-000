ede362e846781bfd095c0d76a850c8aa46cdecd6
Chely Ho <adept_inept@yahoo.com>
2018-10-04 20:58:34 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,13 @@
+import uuid from 'uuid'
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
       const band = {
-        id: Math.random() * 10000000000000000,
+        // id: Math.random() * 10000000000000000,
+        id: uuid(),
         text: action.name
       }
       return { ...state, bands: [...state.bands, band] }
