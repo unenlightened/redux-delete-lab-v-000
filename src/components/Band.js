6e4736a7d22c7a62a2a40382546d9931bf61664d
Chely Ho <adept_inept@yahoo.com>
2018-10-04 20:40:54 +0000

    automatically backed up by learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleClick = event => {
+    event.preventDefault()
+    this.props.delete(this.props.band.id)
+  }
+
   render() {
     return(
       <div>
-        <li>{this.props.band.text} <button onClick={() => this.props.delete(this.props.band.id)}>X</button></li>
+        <li>{this.props.band.text} <button onClick={this.handleClick}>X</button></li>
       </div>
     );
   }
