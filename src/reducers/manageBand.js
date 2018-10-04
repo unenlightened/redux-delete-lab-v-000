af3e24f2b8c23ce24f1a78d9b1c9deb3e07a9f94
Chely Ho <adept_inept@yahoo.com>
2018-10-04 21:13:33 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -7,7 +7,7 @@ export default function manageBand(state = {
         id: Math.random() * 10000000000000000,
         name: action.name
       }
-      return { ...state, bands: state.bands.concat(band) }
+      return { bands: state.bands.concat(band) }
 
     case 'DELETE_BAND':
       return { bands: state.bands.filter(band => band.id !== action.id) }
